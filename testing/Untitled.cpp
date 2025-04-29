e0376635036adaf0fdb1ddf9cefd2ceb896d30f3
EidemsError404 <adam.suchitra@skolakrizik.cz>
2025-04-29 17:36:08 +0200

    pridani 2x  protivnici
    přidáni protivníci jeden na dálku a druhej bez nohou na blízko

--- testing/Untitled.cpp
+++ testing/Untitled.cpp
@@ -3,20 +3,22 @@
 #include <cstdlib>
 using namespace std;
 
-string enst[8][18];
-string zal[8][19];
+string enst[9][18];
+string zal[9][19];
 string hse[8][3];
 string zom[8][1];
+string haz[8][1];
+string plz[8][1];
 string ch[8][1];
 char d;
 int chah = 5;
-int random = 8;
+int random = 12;
 int main(){
 int sp;
 sp = rand() % 20;
-
-
-
+int poc=1;
+int phaz1=1, phaz2=1, phaz3=1;
+int s1, s2, s3;
 
 hse[1][0]="       ";
 hse[1][1]="  /\\   ";
@@ -66,6 +68,38 @@ for (int i=1; i!=9; i++){
 cout << endl;
 cout << endl;
 
+haz[1][0]="       ";
+haz[2][0]="  ___, ";
+haz[3][0]="<|. .|-";
+haz[4][0]=" | = | ";
+haz[5][0]="  \\ / ;";
+haz[6][0]=" ||H|% ";
+haz[7][0]="  |||  ";
+
+for (int i=1; i!=9; i++){
+    cout << haz[i-1][0];
+    cout << endl;
+}
+
+cout << endl;
+cout << endl;
+
+plz[1][0]="       ";
+plz[2][0]="       ";
+plz[3][0]="       ";
+plz[4][0]=" ___   ";
+plz[5][0]="(*=*)  ";
+plz[6][0]=" \\ |_  ";
+plz[7][0]=" =\\__/´";
+
+for (int i=1; i!=9; i++){
+    cout << plz[i-1][0];
+    cout << endl;
+}
+
+cout << endl;
+cout << endl;
+
 ch[1][0]="       ";
 ch[2][0]="       ";
 ch[3][0]="       ";
@@ -79,6 +113,7 @@ for (int i=1; i!=9; i++){
     cout << ch[i-1][0];
     cout << endl;
 }
+
 cout << endl;
 cout << endl;
 
@@ -95,61 +130,106 @@ for (int i=1; i!=9; i++){
 
 
 
-enst[0][17]="|    | ";
-int poc=1;
+
+
 
 
 
 
 do{
-for (int i=1; i!=8; i++){
+for (int i=1; i!=9; i++){
     for (int u=2; u!=19; u++){
         cout << enst[i-1][u-1];
     }
-    if(i<=7){
+    if(i<=8){
     cout << endl;
     }
 }
 for (int i=1; i!=9; i++){
     for (int u=1; u!=19; u++){
-        zal[i-1][u-1]=enst[i-1][u];
+        zal[i][u-1]=enst[i][u];
     }
 }
 for (int i=1; i!=9; i++){
-    enst[i-1][18]= "       ";
+    enst[i][18]= "       ";
 }
 for (int i=1; i!=9; i++){
     for (int u=2; u!=18; u++){
-        enst[i-1][u]=zal[i-1][u];
+        enst[i][u]=zal[i][u];
     }
 }
 if(random<=5){
     for (int i=1; i!=9; i++){
-            enst[i-2][18]=zom[i-1][0];
+            enst[i-1][18]=zom[i-1][0];
     }
 } else if(random>=6 && random<=7){
+    for (int i=1; i!=9; i++){
+            enst[i-1][18]=plz[i-1][0];
+    }
+} else if(random>=8 && random<=9){
+    for (int i=1; i!=9; i++){
+            enst[i-1][18]=haz[i-1][0];
+    }
+} else if(random>=10 && random<=11){
     chah=0;
     for (int i=1; i!=9; i++){
-            enst[i-2][18]=hse[i-1][chah];
+            enst[i-1][18]=hse[i-1][chah];
     }
-} else if(random==8){
+} else if(random==12){
     for (int i=1; i!=9; i++){
-            enst[i-2][18]=ch[i-1][0];
+            enst[i-1][18]=ch[i-1][0];
+            if (phaz1<2){
+                phaz1=18;
+            } else if (phaz2<2){
+                phaz2=18;
+            } else if (phaz3<2){
+                phaz3=18;
+            } else{
+                enst[i-1][18]=zom[i-1][0];
+            }
     }
 }
 cout << "------------------------------------------------------------------------------------------------------------------------";
 if(chah<=2){
     for (int i=1; i!=9; i++){
-            enst[i-2][18]=hse[i-1][chah];
+            enst[i-1][18]=hse[i-1][chah];
     }
 }
 
 random = rand() % 50;
 if(chah<=2){
-    random=120;
+    random=12;
 }
 poc++;
 chah = chah+1;
+
+phaz1--;
+phaz2--;
+phaz3--;
+
+s1=phaz1/2;
+s2=phaz2/2;
+s3=phaz3/2;
+if(s1>1){
+if(s1==s2){
+    if(s1==s3){
+        enst[0][s1-1]= " *  ,  %";
+
+    } else{
+        enst[0][s1-1]= "    ,  %";
+    }
+} else{
+    enst[0][s1-1]= "     %  ";
+}
+} if(s2>1){
+if(s2==s3){
+    enst[0][s1-1]= "     <  ";
+}
+} if(s3>1){
+enst[0][s3-1]= "     <  ";
+}
+
+
 cout << endl << "znovu? f/... ";
 cin >> d;
 } while(d!='f');
