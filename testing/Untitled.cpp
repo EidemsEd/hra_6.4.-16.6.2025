465406f4d4782748e707fffb627cfc2cee3bb263
EidemsError404 <adam.suchitra@skolakrizik.cz>
2025-05-09 00:32:42 +0200

    první možnost rozhodnutí
    první rozhodnutí pro testovací verzi programu
    vylepšení střílení házečů
    zjišťování pozice dumu pro budoucí vylepšení

--- testing/Untitled.cpp
+++ testing/Untitled.cpp
@@ -3,22 +3,53 @@
 #include <cstdlib>
 using namespace std;
 
-string enst[9][18];
-string zal[9][19];
+string enst[10][18];
+string zal[10][19];
+string obj[1][19];
 string hse[8][3];
 string zom[8][1];
 string haz[8][1];
 string plz[8][1];
 string ch[8][1];
 char d;
+char des='s';
+
+int pdes=0;
 int chah = 5;
 int random = 12;
 int main(){
 int sp;
 sp = rand() % 20;
-int poc=1;
-int phaz1=1, phaz2=1, phaz3=1;
-int s1, s2, s3;
+int phaz=0;
+int ihaz[3];
+int pihaz[3];
+int pv=0;
+int cv[5];
+
+float s1, s2, s3;
+
+
+int php=20, mhp=10, mbhp=30, bhp=80;
+int prot=1;
+
+
+
+
+
+
+
+
+
+for (int u=1; u!=4; u++){
+        ihaz[u-1]=0;
+        pihaz[u-1]=0;
+}
+for (int u=1; u!=6; u++){
+        cv[u-1]=0;
+}
+for (int u=1; u!=19; u++){
+        obj[0][u-1]="       ";
+}
 
 hse[1][0]="       ";
 hse[1][1]="  /\\   ";
@@ -48,6 +79,7 @@ for (int i=1; i!=8; i++){
     }
     cout << endl;
 }
+
 
 cout << endl;
 cout << endl;
@@ -87,9 +119,9 @@ cout << endl;
 plz[1][0]="       ";
 plz[2][0]="       ";
 plz[3][0]="       ";
-plz[4][0]=" ___   ";
-plz[5][0]="(*=*)  ";
-plz[6][0]=" \\ |_  ";
+plz[4][0]="       ";
+plz[5][0]=" ___   ";
+plz[6][0]="(*=*)_ ";
 plz[7][0]=" =\\__/´";
 
 for (int i=1; i!=9; i++){
@@ -137,6 +169,10 @@ for (int i=1; i!=9; i++){
 
 
 do{
+for (int u=1; u!=19; u++){
+        cout << obj[0][u-1];
+    }
+cout << endl;
 for (int i=1; i!=9; i++){
     for (int u=2; u!=19; u++){
         cout << enst[i-1][u-1];
@@ -167,27 +203,32 @@ if(random<=5){
             enst[i-1][18]=plz[i-1][0];
     }
 } else if(random>=8 && random<=9){
+    phaz=phaz+1;
+    if(phaz>3){
+        for(int i=1; i!=4;i++){
+            if(ihaz[i-1]<1){
+                phaz=1;
+            }
+        }
+    }
     for (int i=1; i!=9; i++){
             enst[i-1][18]=haz[i-1][0];
     }
+    ihaz[phaz-1]=18;
 } else if(random>=10 && random<=11){
     chah=0;
+    pv=pv+1;
+    if(pv>5){
+        for(int i=1; i!=4;i++){
+            if(cv[i-1]<1){
+                pv=1;
+            }
+        }
+    }
     for (int i=1; i!=9; i++){
             enst[i-1][18]=hse[i-1][chah];
     }
-} else if(random==12){
-    for (int i=1; i!=9; i++){
-            enst[i-1][18]=ch[i-1][0];
-            if (phaz1<2){
-                phaz1=18;
-            } else if (phaz2<2){
-                phaz2=18;
-            } else if (phaz3<2){
-                phaz3=18;
-            } else{
-                enst[i-1][18]=zom[i-1][0];
-            }
-    }
+    cv[pv-1]=18;
 }
 cout << "------------------------------------------------------------------------------------------------------------------------";
 if(chah<=2){
@@ -200,38 +241,82 @@ random = rand() % 50;
 if(chah<=2){
     random=12;
 }
-poc++;
+
 chah = chah+1;
 
-phaz1--;
-phaz2--;
-phaz3--;
 
-s1=phaz1/2;
-s2=phaz2/2;
-s3=phaz3/2;
+obj[0][pihaz[0]]="        ";
+obj[0][pihaz[1]]="        ";
+obj[0][pihaz[2]]="        ";
+
+ihaz[0]--;
+ihaz[1]--;
+ihaz[2]--;
+cv[0]--;
+cv[1]--;
+cv[2]--;
+cv[3]--;
+cv[4]--;
+
+s1=ihaz[0]/2;
+s2=ihaz[1]/2;
+s3=ihaz[2]/2;
+
+pihaz[0]=int(s1)-1;
+pihaz[1]=int(s2)-1;
+pihaz[2]=int(s3)-1;
+
 if(s1>1){
 if(s1==s2){
     if(s1==s3){
-        enst[0][s1-1]= " *  ,  %";
+        obj[0][pihaz[0]]= " *  ,  %";
 
     } else{
-        enst[0][s1-1]= "    ,  %";
+        obj[0][pihaz[1]]= "    ,  %";
     }
 } else{
-    enst[0][s1-1]= "     %  ";
+    obj[0][pihaz[0]]= "     %  ";
 }
 } if(s2>1){
 if(s2==s3){
-    enst[0][s1-1]= "     <  ";
+    obj[0][pihaz[1]]= "     <  ";
 }
 } if(s3>1){
-enst[0][s3-1]= "     <  ";
+obj[0][pihaz[2]]= "     <  ";
 }
-
-
+pdes=4;
+do{
+cout << "Co chces podniknout? " << pdes << endl;
+cout << "- utocit (a), branit (d), healnout se (h); raidnout dum (r), otevrit truhlu(o),spipnout toto a zbyla rozhodnuti(s) -" << endl << "            ";
+cin >> des;
+switch(des){
+    case 'a':
+        cout << "utok" << endl << endl;
+        break;
+    case 'd':
+        cout << "stit" << endl << endl;
+        break;
+    case 'h':
+        cout << "zlate jablko" << endl << endl;
+        break;
+    case 'r':
+        cout << "FBI open up" << endl << endl;
+        break;
+    case 'o':
+        cout << "truhla" << endl << endl;
+        break;
+    case 's':
+        break;
+    default:
+        cout << endl << endl << "bro what the fu*k?" << endl << endl;
+}
+pdes--;
+} while(pdes>1 && des!='s');
 cout << endl << "znovu? f/... ";
 cin >> d;
+
+cout << s1 << ", " << s2 << ", " << s3 << "      " <<  cv[0] << ", " << cv[1] << ", " << cv[2] << ", " <<  cv[3] << ", " << cv[4] << endl;
+
 } while(d!='f');
 cout << "------------------------------------------------------------------------------------------------------------------------";
 }
